453b5b664e8f429872f70a7efa2a6f61dcf58882
Eshita Narayana <eshu0865@gmail.com>
2026-04-12 20:18:46 +0530

    Modified app.py

--- server/app.py
+++ server/app.py
@@ -98,5 +98,10 @@ def state():
 # -------------------------
 # RUN SERVER
 # -------------------------
-if __name__ == "__main__":
+def main():
+    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
